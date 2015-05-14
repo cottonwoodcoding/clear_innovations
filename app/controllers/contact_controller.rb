8fc49072a34a65d7ef58dd92ed4285f4fbd223fa
dave <dave@instructure.com>
2015-05-14 10:17:31 -0600

    fixing mailer

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -1,7 +1,7 @@
 class ContactController < ApplicationController
   def contact_us
     unless Rails.env.development?
-      ContactMailer.send_contact_info(params[:first_name], params[:last_name], params[:email], params[:phone], params[:body])
+      ContactMailer.send_contact_info(params[:first_name], params[:last_name], params[:email], params[:phone], params[:body]).deliver
     end
     redirect_to :root
   end
